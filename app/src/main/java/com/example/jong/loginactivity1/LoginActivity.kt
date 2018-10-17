86e66061c78e3d1c43cf3488fcc5225315c0f7e3
Unknown <kogkongnet@gmail.com>
2018-10-17 18:18:28 +0900

    페이스북 로그인 구현

--- app/src/main/java/com/example/jong/loginactivity1/LoginActivity.kt
+++ app/src/main/java/com/example/jong/loginactivity1/LoginActivity.kt
@@ -4,15 +4,24 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
+import java.util.*
+
 
 class LoginActivity : AppCompatActivity() {
 
@@ -20,6 +29,7 @@ class LoginActivity : AppCompatActivity() {
     var auth: FirebaseAuth? = null
     var googleSignInClient: GoogleSignInClient? = null
     var GOOGLE_LOGIN_CODE = 9001
+    var callbackManager: CallbackManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +40,18 @@ class LoginActivity : AppCompatActivity() {
 
 
 
+        email_login_button.setOnClickListener {
+            createAndLoginEmail()
+        }
+
+        google_sign_in_button.setOnClickListener {
+            googleLogin()
+        }
+
+        facebook_login_button.setOnClickListener {
+            facebookLogin()
+        }
+
         // 구글 로그인 옵션
         var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id)) // 구글 로그인에 접근할 수 있는 인증키
@@ -39,14 +61,9 @@ class LoginActivity : AppCompatActivity() {
         // 구글 로그인 클래스를 만들
         googleSignInClient = GoogleSignIn.getClient(this, gso)
 
+        // 페이스북 ???? 초기화
+        callbackManager = CallbackManager.Factory.create()
 
-        email_login_button.setOnClickListener {
-            createAndLoginEmail()
-        }
-
-        google_sign_in_button.setOnClickListener {
-            googleLogin()
-        }
     }
 
 
@@ -98,6 +115,9 @@ class LoginActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
+        // Facebook SDK로 값 넘겨주기
+        callbackManager?.onActivityResult(requestCode, resultCode, data)
+
         if (resultCode == GOOGLE_LOGIN_CODE) {
             var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data) // 구글에서 성공된 데이터 가 넘어옴
 
@@ -107,6 +127,40 @@ class LoginActivity : AppCompatActivity() {
             }
         }
     }
+
+
+    fun facebookLogin() {
+        LoginManager.getInstance()
+            .logInWithReadPermissions(this, Arrays.asList("public_profile", "email"))
+        LoginManager
+            .getInstance().registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult?) {
+                    handleFacebookAccessToken(result?.accessToken)
+                }
+
+                override fun onCancel() {
+
+                }
+
+                override fun onError(error: FacebookException?) {
+
+                }
+
+
+            })
+    }
+
+    fun handleFacebookAccessToken(token:AccessToken?) {
+        var credential = FacebookAuthProvider.getCredential(token?.token!!)
+        auth?.signInWithCredential(credential)?.addOnCompleteListener {
+            task ->
+            if(task.isSuccessful) {
+//                moveMainPage(auth?.currentUser)
+            }
+        }
+
+    }
+
 
     fun firebaseAuthWithGoogle(account: GoogleSignInAccount) {
         var credential = GoogleAuthProvider.getCredential(account.idToken, null)
